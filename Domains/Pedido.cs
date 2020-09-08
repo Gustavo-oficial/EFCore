ff66fe0f6a554c105db1fdc922061a5667ffa0f0
unknown <gmandelinha@gmail.com>
2020-09-08 11:52:22 -0300

    primeira ORM

---
+++ Domains/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ORM.EFCore.Domains
+{
+    public class Pedido
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public Pedido()
+        {
+            Id = Guid.NewGuid();
+        }
+
+
+    }
+}
---
+++ PedidoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ORM.EFCore.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ORM.EFCore
+{
+    public class PedidoContext : DbContext
+    {
+        public DbSet<PedidoItem> PedidoItem { get; set; }
+        public DbSet<Pedido> Pedido { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS;Initial Catalog=loja;User ID=sa;Password=sa132");
+        }
+    }
+}
